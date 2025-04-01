19ac45c96c6986247ab0b0c09cb6ecaf3672b5aa
sacm <sacm1046@gmail.com>
2025-04-01 20:44:18 -0300

    errorBoundary added

--- src/federatedComponents/actions/index.jsx
+++ src/federatedComponents/actions/index.jsx
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from 'react';
 import Loader from '../../components/loader';
+import ErrorBoundary from '../../components/errorBoundary';
 const RemoteActions = lazy(() => import('remoteApp/Actions'));
 
 export default function FederatedActions() {
-    return <Suspense fallback={<Loader />}>
-        <RemoteActions />
-    </Suspense>
+    return <ErrorBoundary>
+        <Suspense fallback={<Loader />}>
+            <RemoteActions />
+        </Suspense>
+    </ErrorBoundary>
 }
--- src/federatedComponents/main/index.jsx
+++ src/federatedComponents/main/index.jsx
@@ -1,9 +1,12 @@
 import { lazy, Suspense } from 'react';
 import Loader from '../../components/loader';
+import ErrorBoundary from '../../components/errorBoundary';
 const RemoteMain = lazy(() => import('remoteApp/Main'));
 
 export default function FederatedMain() {
-    return <Suspense fallback={<Loader />}>
-        <RemoteMain />
-    </Suspense>
+    return <ErrorBoundary>
+        <Suspense fallback={<Loader />}>
+            <RemoteMain />
+        </Suspense>
+    </ErrorBoundary>
 }
